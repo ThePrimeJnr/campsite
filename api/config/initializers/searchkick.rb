976e8cde4ca126a9bc89b071f49d03ad1473accf
Pablo Masi <pablomasia1015@gmail.com>
2025-01-23 03:19:07 +0000

    final

--- api/config/initializers/searchkick.rb
+++ api/config/initializers/searchkick.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ENV["ELASTICSEARCH_URL"] = if Rails.env.production?
-  "http://campsite-elasticsearch.internal:9200"
+  "http://campsite-elasticsearch-dessn.internal:9200"
 else
   "http://localhost:9200"
 end
